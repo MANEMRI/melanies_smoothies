c879341ea69226f21d60b1daf4b1f16c45a7ba71
MANEMRI <mohamed.amine.nemri@devoteam.com>
2026-02-09 15:49:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # convert the dnowpark df to a pandas df so we can use the loc function.
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 
 ingredients_list= st.multiselect(
@@ -37,7 +37,13 @@ if ingredients_list:
      ingredients_string=''
      for fruit_chosen in ingredients_list:
          ingredients_string+= fruit_chosen+' '
-         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
+         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
+         st.subheader(fruit_chosen+' Nutrition Information')
+         fruitvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/all/"+fruit_chosen)
+       
+         # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
          sd_df= st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
      st.write(ingredients_string)
      my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
