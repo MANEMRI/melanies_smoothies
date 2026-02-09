26f81844724eab1581362d0863513ad42d66ed1c
MANEMRI <mohamed.amine.nemri@devoteam.com>
2026-02-09 15:21:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 
 # Write directly to the app
@@ -29,6 +30,8 @@ if ingredients_list:
      ingredients_string=''
      for fruit_chosen in ingredients_list:
          ingredients_string+= fruit_chosen+' '
+         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+         sd_df= st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
      st.write(ingredients_string)
      my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
@@ -40,16 +43,4 @@ if ingredients_list:
      if time_to_insert:
          session.sql(my_insert_stmt).collect()
          st.success('Your Smoothie is ordered!', icon="✅")
-       
-# new section to display smoothiefroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sd_df= st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
-# option = st.selectbox(
-#     "What is your favorite fruit?",
-#     ("Banana", "Strawberries", "Peaches"),
-# )
-
-# st.write("Your favorite fruit is:", option)
